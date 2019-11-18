a6a7c0cb4a38ac1bc49bbc10fdc8f1c083736c06
gcalexandre <gcalexandre.castilho@gmail.com>
2019-11-18 05:50:17 -0200

    Service Layer, component Registration

--- src/main/java/com/alexandrecasrtilho/course/repositories/UserRepository.java
+++ src/main/java/com/alexandrecasrtilho/course/repositories/UserRepository.java
@@ -2,9 +2,11 @@ package com.alexandrecasrtilho.course.repositories;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.alexandrecasrtilho.course.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 
 	
