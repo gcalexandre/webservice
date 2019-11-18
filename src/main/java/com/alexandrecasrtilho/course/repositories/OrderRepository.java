c0a9b1eceba29cb6bbf54bb4c1621f244d0f3ea6
gcalexandre <gcalexandre.castilho@gmail.com>
2019-11-18 08:34:09 -0200

    Order, Instant, ISO 8601

---
+++ src/main/java/com/alexandrecasrtilho/course/repositories/OrderRepository.java
@@ -0,0 +1,14 @@
+package com.alexandrecasrtilho.course.repositories;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.alexandrecasrtilho.course.entities.Order;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+	
+
+}
