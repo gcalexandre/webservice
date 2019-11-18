adc0ce5556131efcb87d0852292834aa0ec433d8
gcalexandre <gcalexandre.castilho@gmail.com>
2019-11-18 10:07:18 -0200

    OrderStatus enum

--- src/main/java/com/alexandrecasrtilho/course/entities/Order.java
+++ src/main/java/com/alexandrecasrtilho/course/entities/Order.java
@@ -10,31 +10,35 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.alexandrecasrtilho.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 @Table(name = "tb_order")
 public class Order {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
-	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
-	
+
+	private Integer orderStatus;
+
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
-	
+
 	public Order() {
-		
+
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -52,6 +56,16 @@ public class Order {
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+
+	public OrderStatus getOrderstatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderstatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderstatus.getCode();
+		}
 	}
 
 	public User getClient() {
@@ -83,9 +97,5 @@ public class Order {
 			return false;
 		return true;
 	}
-	
-	
-
-	
 
 }
